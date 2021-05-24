ab038d2dd602b9a2309b5924c248a58df9b730d4
ch-pat <patryk.choma@gmail.com>
2021-05-24 17:43:40 +0200

    added subreddit query

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 import dynamo
-
-#name = "ciao"
-
-#app = Flask(__name__)
-
-#@app.route("/")
-#def pagina():
-#    return render_template('index.html', name=name)
 
 DEBUG = True
 app = Flask(__name__)
@@ -21,22 +13,27 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 Bootstrap(app)
 
 class SearchForm(FlaskForm):
-    search = StringField('Search', validators=[DataRequired()])
+    choices = dynamo.subreddits()
+    subreddit = SelectField('Subreddit', choices=choices)
+    search = StringField('Search')
     submit = SubmitField('Submit')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # you must tell the variable 'form' what you named the class, above
     # 'form' is the variable name used in this template: index.html
     form = SearchForm()
-    message = ""  # AL POSTO DI MESSAGE PREPARA I DATI PER LA TABELLA -- mettici gli ultimi x post
     if form.validate_on_submit():
         search = form.search.data
-        # FAI LA QUERY QUA
-        table_contents = dynamo.query_table_by_title(search)
+        subreddit = form.subreddit.data
+        # TODO: fai qualcosa per migliorare la ricerca che non da risultati
+        table_contents = dynamo.query_table_by_title(search, subreddit)
         # empty the form field
         form.search.data = ""
+    else:
+        table_contents = dynamo.default_query()
 
-    return render_template('index.html', form=form, message=message, table_contents=table_contents)
+    return render_template('index.html', form=form, table_contents=table_contents)
 
 app.run(debug=True)
--- dynamo.py
+++ dynamo.py
@@ -1,5 +1,6 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
+import os
 
 
 TABLE_NAME = 'posts'
@@ -46,10 +47,11 @@ def create_posts_table():
     return table
 
 
-def query_table_by_title(query_string: str):
+def query_table_by_title(query_string: str, subreddit: str):
     table = get_posts_table()
     response = table.scan(
         FilterExpression=Attr('title').contains(query_string)
+        & Attr('subreddit').eq(subreddit)
     )
     results = []
     items = response['Items']
@@ -63,6 +65,62 @@ def query_table_by_title(query_string: str):
         row = [title, subreddit, url, num_comments, timestamp, name]
         results += [row]
     return results
+
+def default_query():
+    table = get_posts_table()
+    response = table.scan(
+        Limit=20
+    )
+    results = []
+    items = response['Items']
+    for item in items:
+        url = item['url']
+        name = item['name']
+        num_comments = item['num_comments']
+        title = item['title']
+        timestamp = item['timestamp']
+        subreddit = item['subreddit']
+        row = [title, subreddit, url, num_comments, timestamp, name]
+        results += [row]
+    return results
+
+def subreddits():
+    if os.path.isfile('subreddits'):
+        with open('subreddits', 'r', encoding='utf-8') as f:
+            results = f.readlines()
+
+        return [r[:-1] for r in results]
+    else:
+        table = get_posts_table()
+        results = []
+        items = []
+        last_evaluated_key = None
+
+        # Get all items from DB !!! takes a long time !!!
+        while True:
+            if last_evaluated_key:
+                response = table.scan(
+                    ExclusiveStartKey=last_evaluated_key
+                )
+            else: 
+                response = table.scan()
+            last_evaluated_key = response.get('LastEvaluatedKey')
+            
+            items.extend(response['Items'])
+            
+            if not last_evaluated_key:
+                break
+        ######
+
+
+        for item in items:
+            subreddit = f"{item['subreddit']}\n"
+            if subreddit not in results:
+                results += [subreddit]
+        results = sorted(results)
+        with open('subreddits', 'w+', encoding='utf-8') as f:
+            f.writelines(results)
+        return results
 
 if __name__ == "__main__":
     table = get_posts_table()
