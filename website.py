1b8c213ccf813d1137f6cc7503f517190ab6d3a2
ch-pat <patryk.choma@gmail.com>
2021-06-01 23:24:36 +0200

    add wordcloud

--- website.py
+++ website.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import dynamo
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 def find_subreddit(search: str) -> str:
     search = search.lower()
@@ -20,6 +21,12 @@ def extract_top(sub_counts: list, limit: int) -> (list, list):
         names += [t[0]]
         counts += [t[1]]
     return names, counts
+
+def extract_string_from_titles(titles: pd.DataFrame) -> str:
+    result = ""
+    for title in titles:
+        result += f" {str(title)}"
+    return result
 
 
 with st.form(key='search_form'):
@@ -40,12 +47,24 @@ if submit_button:
     headings = ['Title', 'Subreddit', 'url', 'timestamp', 'name']
     table = pd.DataFrame(table_contents)
     table.columns = headings
+
+    titles = extract_string_from_titles(table['Title'])
+    wordcloud = WordCloud().generate(titles)
+
     st.write(table)
+
+    # Display wordcloud
+    st.markdown("<center>Most used words Word Cloud</center>", unsafe_allow_html=True)
+    fig, ax = plt.subplots(1, 1)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    st.pyplot(fig)
 else:
     # Pagina principale prima della ricerca
     sub_counts = dynamo.get_subreddit_counts()
     names, counts = extract_top(sub_counts, limit=30)
     
+    # Display popular subreddits chart
     fig, ax = plt.subplots(1, 1)
     ax.bar(names, counts)
     ax.set_title("Top Subreddits")
